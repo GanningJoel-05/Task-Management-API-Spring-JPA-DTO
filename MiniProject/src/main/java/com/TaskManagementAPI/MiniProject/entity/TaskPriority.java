facc7e157e7d9977b00448f1a2d16b7e717e9b86
Ganning Joel J <ganningjoel169@gmail.com>
2026-02-25 09:17:15 +0530

    Delete MiniProject directory

--- MiniProject/src/main/java/com/TaskManagementAPI/MiniProject/entity/TaskPriority.java
+++
@@ -1,7 +0,0 @@
-package com.TaskManagementAPI.MiniProject.entity;
-
-public enum TaskPriority {
-    LOW,
-    MEDIUM,
-    HIGH
-}
--- MiniProject/src/main/java/com/TaskManagementAPI/MiniProject/entity/TaskStatus.java
+++
@@ -1,7 +0,0 @@
-package com.TaskManagementAPI.MiniProject.entity;
-
-public enum TaskStatus {
-    TODO,
-    IN_PROGRESS,
-    DONE
-}
